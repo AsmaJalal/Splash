70d108d1e3dd87e9a0d71a44a9baf5fe1cb7fd74
Asma772 <asmaarain098@gmail.com>
2022-11-26 10:14:30 +0500

    splash app tested

---
+++ app/src/main/java/com/asma/splash/MainActivity.java
@@ -0,0 +1,31 @@
+package com.asma.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*
+        This activity is created and opened when SplashScreen finishes its animations.
+        To ensure a smooth transition between activities, the activity creation animation
+        is removed.
+        RelativeLayout with EditTexts and Button is animated with a default fade in.
+         */
+
+        overridePendingTransition(0, 0);
+        View relativeLayout = findViewById(R.id.login_container);
+        Animation animation = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        relativeLayout.startAnimation(animation);
+
+    }
+}
